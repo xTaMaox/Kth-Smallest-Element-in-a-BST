e4f4bd2f5144db1f30278091c41a5c6a0b362033
Muhamad Adi Prasetyo <88260025+xTaMaox@users.noreply.github.com>
2023-02-22 22:55:49 +0700

    Add files via upload

---
+++ Kth Smallest Element in a BST.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func walk(root *TreeNode, c chan int) {
+	if root == nil {
+		return
+	}
+
+	walk(root.Left, c)
+	c <- root.Val
+	walk(root.Right, c)
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	c := make(chan int)
+	go walk(root, c)
+
+	for i := 0; i < k-1; i++ {
+		<-c
+	}
+	return <-c
+}
